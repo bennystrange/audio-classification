92743fe9e5b0f2275b286705e8a8cac938532217
Matthew Nutt <matthewcnutt@gmail.com>
2025-04-19 13:31:16 -0500

    fixed fft extraction, added better print statements, added automatic directory, cleaned up misc

--- 378final.py
+++ 378final.py
@@ -1,23 +1,30 @@
 import os
-import librosa
+import csv
 import numpy as np
-import matplotlib.pyplot as plt
-#from scipy.spatial.distance import cdist
-#from scipy.stats import mode
 from scipy.signal import welch
 from scipy.fft import fft, fftfreq
+import librosa
+import matplotlib.pyplot as plt
 
-import csv
 from sklearn.model_selection import train_test_split
-
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import StratifiedKFold
 from sklearn.decomposition import PCA
 
-from google.colab import drive
-drive.mount('/content/drive')
+# in theory, this should automagically detect whether or not you're in colab and set the directory accordingly
+# if in colab, put link to shared 378final folder in My Drive
+# if local, make dataset folder in same directory as .py file containing .wav folders
+# we won't track dataset folder on Git because it's 2GB Lol.
+try:
+    from google.colab import drive
+    drive.mount("/content/drive")
+    dataset_directory = "/content/drive/My Drive/378final/dataset"
+except:
+    dataset_directory = "./dataset"
+
+
 
 """Data Exploration (FFT & PSD)"""
 
@@ -27,13 +34,13 @@ labels = ["Angry", "Disgusted", "Fearful", "Happy", "Neutral", "Sad", "Suprised"
 """
 # we would like to get a sense of the signal's properties so see what are some good features to extract
 for i in range(len(labels)):
-    file_name = f'/content/drive/My Drive/378final/Dataset/train/train{str(i).zfill(3)}.wav'
+    file_name = f"/content/drive/My Drive/378final/Dataset/train/train{str(i).zfill(3)}.wav"
     y, sr = librosa.load(file_name, sr=None)  # load full audio signal
 
     # raw audio waveform
     # Plot the waveform
     plt.figure(figsize=(8, 6))
-    librosa.display.waveshow(y, sr=sr, color = 'green')
+    librosa.display.waveshow(y, sr=sr, color = "green")
     plt.title("Audio Time Series (Waveform)")
     plt.xlabel("Time (s)")
     plt.ylabel("Amplitude")
@@ -44,7 +51,7 @@ for i in range(len(labels)):
     yf = fft(y)
     xf = fftfreq(N, 1 / sr)[:N // 2]
     plt.figure(figsize=(8, 6))
-    plt.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]), color = 'blue')
+    plt.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]), color = "blue")
     plt.title(f"FFT of File {i}")
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Amplitude")
@@ -54,7 +61,7 @@ for i in range(len(labels)):
     # psd
     f, Pxx = welch(y, sr, nperseg=1024)
     plt.figure(figsize=(8, 6))
-    plt.semilogy(f, Pxx, color = 'orange')
+    plt.semilogy(f, Pxx, color = "orange")
     plt.title(f"PSD of File {i}")
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Power Spectral Density")
@@ -62,25 +69,30 @@ for i in range(len(labels)):
     plt.show()
 """
 
+
+
 """
 The basic data exploration above provides intuition for extracting features or processing raw data to be in a different format that might allow for better ML models (think linear separability).
 
 Feature Extraction (per audio signal)
 """
-
 def extract_features(file_path, standard_audio_size=4):
     try:
         # Load the audio file
-        audio_timeseries, sampling_rate = librosa.load(file_path, sr=None)
-        padded_audio = librosa.util.fix_length(audio_timeseries, size=standard_audio_size*sampling_rate) # array size is required_audio_size*sampling frequency
+        # Apparently the sampling rate differs between files, so we fix it here
+        audio, sampling_rate = librosa.load(file_path, sr=8000)
+
+        # Pad/truncate to get standard length (so features are consistent length)
+        padded_audio = librosa.util.fix_length(audio, size=standard_audio_size*sampling_rate)
 
         # Extract features
-        fft_freqs = librosa.fft_frequencies(sr=sampling_rate, n_fft=512)
+        fft_coeffs = fft(padded_audio)
+        # fft_freqs = fftfreq(standard_audio_size*sampling_rate, 1 / sampling_rate)
         #mfccs = librosa.feature.mfcc(y=audio_timeseries, sr=sampling_rate, n_mfcc=512)
         #melspec = gen_melspec(file, n_mels=)
         #chroma = librosa.feature.chroma_stft(y=audio_timeseries, sr=sampling_rate)
-        #all_features = np.vstack([fft_freqs, mfccs])
-        all_features = fft_freqs
+        # all_features = np.vstack([fft_coeffs])
+        all_features = np.abs(fft_coeffs)
         #librosa_features_mean = np.mean(all_features, axis=1)
         return all_features
     except Exception as e:
@@ -91,35 +103,31 @@ def extract_features(file_path, standard_audio_size=4):
 #Another Note: This is an example of feature extraction per data point (signal). You could also do something like PCA on all the raw
 #               train data (as in directly on the audio signals) or PCA on all data as the computed features per datapoint.
 
-"""Audio as Melspectograms (Hint Hint for CNNs, other image classifiers)"""
+#Audio as Melspectograms (Hint Hint for CNNs, other image classifiers)
 
+"""
 def gen_melspec(file, n_mels=32):
   audio_timeseries, sampling_rate = librosa.load(file, sr=None)
   mel_spec = librosa.feature.melspectrogram(y=audio_timeseries, sr=sampling_rate, n_mels=n_mels)
   log_mel_spec = librosa.power_to_db(mel_spec, ref=np.max)
   return log_mel_spec.flatten()
   # there are also other features that you can explore too!
+"""
+
+
 
 """Loading Features"""
+print("---Extracting Train Data---")
 
 # Load dataset and extract features
 features = []
 labels_for_features = []
 
-"""
-# We had long audio files, so specify duration and num of segments for splitting these long audio files
-total_duration = 30
-segment_duration = 3
-segments_per_file = total_duration // segment_duration
-"""
-
-print("Extracting Features")
 for label in labels:
-    for num, file_name in enumerate(os.listdir(f"/content/drive/My Drive/378final/{label}")):
-        if (num < 100): # remove this to read ALL files
-            if num % 10 == 0:
-                print(num)
-            file_path = f"/content/drive/My Drive/378final/{label}/{file_name}"
+    print(f"Extracting: {label}")
+    for num, file_name in enumerate(os.listdir(f"{dataset_directory}/{label}")):
+        #if (num < 100): # comment this out this to use ALL files
+            file_path = f"{dataset_directory}/{label}/{file_name}"
             extracted_features = extract_features(file_path)
             if extracted_features is not None:
                 features.append(extracted_features)
@@ -128,7 +136,7 @@ for label in labels:
 
 
 """Model + Trainer"""
-print("Training model I think")
+print("---Training Model---")
 
 # Convert to numpy arrays and continue as before
 features = np.array(features)
@@ -138,28 +146,31 @@ labels_for_features = np.array(labels_for_features)
 label_encoder = LabelEncoder()
 labels_encoded = label_encoder.fit_transform(labels_for_features)
 
-#print('encoded labels', labels_encoded)
+#print("encoded labels", labels_encoded)
 
 # Initialize cross-validation (you can use this or simple train test split although this will make testing of your model more robust!)
 kf = StratifiedKFold(n_splits=5, random_state=42, shuffle=True)
 accuracies = []
 
-'''
+"""
 Alternatively, you can also simply use sklearn's train_test_split function!! Cross validation is K different random combinations
 of this train and test data split in order to train the model more robustly and ultimately use all your available data for the kaggle competition.
-'''
+"""
 
 # SVM Model and preprocessing initialization
-svm_clf = SVC(kernel='rbf', C=1, gamma='auto')
+svm_clf = SVC(kernel="rbf", C=1, gamma="auto")
 scaler = StandardScaler()
 pca = PCA(n_components=0.95) #keep enough components to preserve 95% of the variance in the data
 
 # Perform cross-validation
-for train_index, test_index in kf.split(features, labels_encoded):
+for i, (train_index, test_index) in enumerate(kf.split(features, labels_encoded)):
+    print(f"Split #{i+1}")
     X_train, X_test = features[train_index], features[test_index]
     y_train, y_test = labels_encoded[train_index], labels_encoded[test_index]
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
+    # NOTE: PCA IS A MAJOR SLOWDOWN! THE BIGGER THE DATASET, THE LONGER IT TAKES!
+    # fitting the svm is genuinely faster than the pca
     X_train_pca = pca.fit_transform(X_train_scaled)
     X_test_pca = pca.transform(X_test_scaled)
     svm_clf.fit(X_train_pca, y_train)
@@ -170,27 +181,30 @@ for train_index, test_index in kf.split(features, labels_encoded):
 average_accuracy = np.mean(accuracies)
 print(f"Average Accuracy: {average_accuracy * 100:.2f}%")
 
+
+
 """Loading and Testing Test Data"""
-print("Making predictions")
+print("---Extracting Test Data---")
 
 test = []
-#duration = 10 #we were given 10 sec long test clips
-#segment_duration = 3
-#segments_per_file = 3
-
 test_features = []
+num_test = len(os.listdir(f"{dataset_directory}/Test"))
+checkpoints = []
+for i in range(5):
+    checkpoints.append(round(i*num_test/4))
 
-for i, file_name in enumerate(os.listdir(f"/content/drive/My Drive/378final/Test")):
-    if i % 10 == 0:
-        print(i)
-    file_path = f"/content/drive/My Drive/378final/Test/{file_name}"
+for i, file_name in enumerate(os.listdir(f"{dataset_directory}/Test")):
+    for num in checkpoints:
+        if i == num:
+            print(f"Extraction {int(100*checkpoints.index(num)/4)}% complete")
+    file_path = f"{dataset_directory}/Test/{file_name}"
     the_thing = extract_features(file_path)
     if the_thing is not None:
         test_features.append(the_thing.reshape(1, -1))
 
 """
 for i in range(20):  # Assuming 20 test files
-    file_name = f'/content/drive/My Drive/Elec378 Final Project/Dataset/test/test{str(i).zfill(3)}.wav'
+    file_name = f"/content/drive/My Drive/Elec378 Final Project/Dataset/test/test{str(i).zfill(3)}.wav"
     for j in range(segments_per_file):
         start_second = j * segment_duration
         mfccs = extract_features(file_name, start_second, duration=segment_duration)
@@ -198,6 +212,8 @@ for i in range(20):  # Assuming 20 test files
             #2D array shape
             test_features.append(mfccs.reshape(1, -1))
 """
+
+print("---Making Predictions---")
             
 # Convert list of arrays into a single numpy array
 test_features = np.vstack(test_features)
@@ -208,13 +224,13 @@ test_pca = pca.transform(test_scaled)
 test_pred = svm_clf.predict(test_pca)
 test_pred_labels = label_encoder.inverse_transform(test_pred)
 
-
 # Save to a CSV for submission to Kaggle
-with open('svm.csv', 'w', newline='', encoding='utf-8') as f:
+with open("svm.csv", 'w', newline='', encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerow(['filename', 'emotion'])
+    writer.writerow(["filename", "emotion"])
     for i, emotion in enumerate(test_pred_labels):
         writer.writerow([f"{str(i+1)}.wav", emotion])
+
 
 
 """Reducing Overfitting through Recursive Feature Extraction(RFE)
@@ -226,6 +242,7 @@ feature is removed recursively. The model is then refitted with the new, reduced
 process is iterated until the specified number of features is reached.
 """
 
+"""
 import numpy as np
 import joblib
 from sklearn.preprocessing import StandardScaler, LabelEncoder
@@ -266,23 +283,24 @@ def train_svm_classifier(features, labels):
     X_val_rfe = rfe.transform(X_val)
 
     # Train the final model using the RBF kernel with selected features
-    model = SVC(kernel='rbf', C=1, decision_function_shape='ovo', gamma='scale')
+    model = SVC(kernel="rbf", C=1, decision_function_shape="ovo", gamma="scale")
     model.fit(X_train_rfe, y_train)
 
     # Predict on the validation set with the reduced feature set
     y_pred = model.predict(X_val_rfe)
     validation_accuracy = accuracy_score(y_val, y_pred)
-    print(f'Validation Accuracy: {validation_accuracy}')
+    print(f"Validation Accuracy: {validation_accuracy}")
 
     # Save the best model, scaler, encoder, and RFE selector
-    joblib.dump(model, 'Dataset/svm_model_optimal_rfe.joblib')
-    joblib.dump(scaler, 'Dataset/scaler_optimal_rfe.joblib')
-    joblib.dump(encoder, 'Dataset/label_encoder_optimal_rfe.joblib')
-    joblib.dump(rfe, 'Dataset/rfe_selector_optimal.joblib')
+    joblib.dump(model, "Dataset/svm_model_optimal_rfe.joblib")
+    joblib.dump(scaler, "Dataset/scaler_optimal_rfe.joblib")
+    joblib.dump(encoder, "Dataset/label_encoder_optimal_rfe.joblib")
+    joblib.dump(rfe, "Dataset/rfe_selector_optimal.joblib")
 
 if __name__ == "__main__":
-    features, labels = load_data('train_features.npy', 'train_labels.npy')
+    features, labels = load_data("train_features.npy", "train_labels.npy")
     train_svm_classifier(features, labels)
+"""
 
 """Note on Overfitting:
 Note how we tried to mitigate the overfitting problem explicitly at the end through RFE but also implicitly at several prior points in the data science pipeline. We used PCA to capture 95% variance and thus eliminate highly specfic dimensions in feature space that could potentially cause the model to overfit. We also used K-fold cross validation instead of a simple train test split in order to not make training dependent on just one random split of the available data.
